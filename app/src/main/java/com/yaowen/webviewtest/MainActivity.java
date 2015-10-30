9952bb6b39aa497d7f4b7536141ab5181e5f172f
茗香墨语 <yaowen424@126.com>
2015-10-30 11:02:23 +0800

    完成了主活动的代码

--- app/src/main/java/com/yaowen/webviewtest/MainActivity.java
+++ app/src/main/java/com/yaowen/webviewtest/MainActivity.java
@@ -2,12 +2,25 @@ package com.yaowen.webviewtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        webView = (WebView) findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                webView.loadUrl(url);// 根据传⼊的参数再去加载新的⺴⻚
+                return true;// 表⽰当前WebView可以处理打开新网络的请求，不⽤借助系统浏览器
+            }
+        });
+        webView.loadUrl("https://www.baidu.com");
     }
 }
